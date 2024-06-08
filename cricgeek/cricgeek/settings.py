b15ce8de105811275a2a32fa0d6c32447b8d9897
Aditya-Uppuluri <49834918+Aditya-Uppuluri@users.noreply.github.com>
2024-06-08 13:29:44 +0530

    Update settings.py

--- cricgeek/cricgeek/settings.py
+++ cricgeek/cricgeek/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!4l&s#!a1a-ypgk455jt(_ce9w4ya-h!94+a*r#8064mqv859d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
