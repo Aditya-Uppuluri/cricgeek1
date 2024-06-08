bf88f42ba076586fb2b0827fabdd3bb6bfaa78c2
Aditya-Uppuluri <abhi.uppuluri@gmail.com>
2024-06-09 00:36:21 +0530

    fixed bugs

--- cricgeek/users/models.py
+++ cricgeek/users/models.py
@@ -3,6 +3,25 @@ from django.contrib.auth.models import User
 from PIL import Image
 
 
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     image = models.ImageField(default='/Users/adityauppuluri/Desktop/cricgeek1/cricgeek/profile_pics/FINAL_IMAGE_62kB.jpg', upload_to='profile_pics')
+
+#     def __str__(self):
+#         return f'{self.user.username} Profile'
+
+#     def save(self, *args, **kwargs):
+#         super().save(*args, **kwargs)
+
+#         img = Image.open(self.image.path)
+
+#         if img.height > 300 or img.width > 300:
+#             output_size = (300, 300)
+#             img.thumbnail(output_size)
+#             img.save(self.image.path)
+
+import os
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
@@ -10,12 +29,18 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+        img_path = self.image.path
+        img_dir = os.path.dirname(img_path)
+        
+        if not os.path.exists(img_dir):
+            os.makedirs(img_dir)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+        if os.path.exists(img_path):
+            img = Image.open(img_path)
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(img_path)
